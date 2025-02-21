76b37dc45f8b89db282f1fb9d3729d5418645f63
Lauri Alanen <laurialanen1@gmail.com>
2025-02-21 13:20:25 +0200

    Added systick initalization

--- Core/App/app_main.c
+++ Core/App/app_main.c
@@ -7,16 +7,24 @@
 static OS_TCB App_TaskStartTCB;
 static CPU_STK App_TaskStartStk[TASK_STK_SIZE];
 
-#define BLINK_TASK_PRIORITY 11
-static OS_TCB App_TaskBlinkTCB;
-static CPU_STK App_TaskBlinkStk[TASK_STK_SIZE];
+#define BLINK_TASK_PRIORITY_1 11
+static OS_TCB App_TaskBlink1TCB;
+static CPU_STK App_TaskBlink1Stk[TASK_STK_SIZE];
+
+#define BLINK_TASK_PRIORITY_2 12
+static OS_TCB App_TaskBlink2TCB;
+static CPU_STK App_TaskBlink2Stk[TASK_STK_SIZE];
 
 static void App_TaskStart(void *p_arg);
-static void App_TaskBlink(void *p_arg);
+static void App_TaskBlink1(void *p_arg);
+static void App_TaskBlink2(void *p_arg);
 
-int app_main()
+int main()
 {
     OS_ERR err;
+
+    Device_Init();
+
     OSInit(&err);
 
     OSTaskCreate((OS_TCB *)&App_TaskStartTCB,
@@ -34,24 +42,40 @@ int app_main()
                 (OS_ERR *)&err);
 
     OSStart(&err);  
-
+    
     while (DEF_ON){}
     
     return 0;
-}
+}   
 
 static void App_TaskStart(void *p_arg)
-{
+{   
     OS_ERR err;
 
     p_arg = p_arg;
 
-    OSTaskCreate((OS_TCB *)&App_TaskBlinkTCB,
-                (CPU_CHAR *)"App Task Blink",
-                (OS_TASK_PTR) App_TaskBlink,
+    OS_CPU_SysTickInit(0x11940);
+
+    OSTaskCreate((OS_TCB *)&App_TaskBlink1TCB,
+                (CPU_CHAR *)"App Task Blink 1",
+                (OS_TASK_PTR) App_TaskBlink1,
                 (void *) 0,
-                (OS_PRIO) BLINK_TASK_PRIORITY,
-                (CPU_STK *)&App_TaskBlinkStk[0],
+                (OS_PRIO) BLINK_TASK_PRIORITY_1,
+                (CPU_STK *)&App_TaskBlink1Stk[0],
+                (CPU_STK_SIZE)(TASK_STK_SIZE / 10u),
+                (CPU_STK_SIZE) TASK_STK_SIZE,
+                (OS_MSG_QTY) 0,
+                (OS_TICK) 0,
+                (void *) 0,
+                (OS_OPT)(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+                (OS_ERR *)&err);
+    
+    OSTaskCreate((OS_TCB *)&App_TaskBlink2TCB,
+                (CPU_CHAR *)"App Task Blink 2",
+                (OS_TASK_PTR) App_TaskBlink2,
+                (void *) 0,
+                (OS_PRIO) BLINK_TASK_PRIORITY_2,
+                (CPU_STK *)&App_TaskBlink2Stk[0],
                 (CPU_STK_SIZE)(TASK_STK_SIZE / 10u),
                 (CPU_STK_SIZE) TASK_STK_SIZE,
                 (OS_MSG_QTY) 0,
@@ -68,7 +92,7 @@ static void App_TaskStart(void *p_arg)
     }
 }
 
-static void App_TaskBlink(void *p_arg)
+static void App_TaskBlink1(void *p_arg)
 {
     OS_ERR err;
 
@@ -77,6 +101,21 @@ static void App_TaskBlink(void *p_arg)
     while (DEF_ON)
     {
         HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+        OSTimeDlyHMSM(0u, 0u, 0u, 500u,
+            OS_OPT_TIME_HMSM_STRICT,
+            &err);
+    }
+}
+
+static void App_TaskBlink2(void *p_arg)
+{
+    OS_ERR err;
+
+    p_arg = p_arg;
+
+    while (DEF_ON)
+    {
+        HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_6);
         OSTimeDlyHMSM(0u, 0u, 0u, 500u,
             OS_OPT_TIME_HMSM_STRICT,
             &err);
